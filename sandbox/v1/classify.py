1ce71bf1248e03b3bef5cf375ad234775df1f3ec
evanthebouncy <evanthebouncy@gmail.com>
2018-09-25 01:31:36 -0400

    looks promising

---
+++ sandbox/v1/classify.py
@@ -0,0 +1,138 @@
+from sklearn.linear_model import LogisticRegression
+import numpy as np
+from data import make_dataset, L
+import matplotlib.pyplot as plt
+
+
+# =========================== UTILITIES =============================
+# feature extraction made easy when u can cheat :D
+def feature(x):
+  return x[:L]
+
+# do a 2D visualization of the space of things
+def tsne(embedded_X, labels=None, name=None):
+  cl_colors = np.linspace(0, 1, len(labels)) if (labels is not None) else ['blue']
+  from sklearn.manifold import TSNE
+  X_tsne = TSNE(n_components=2).fit_transform(embedded_X)
+  # import matplotlib
+  # matplotlib.use("svg")
+  x = [x[0] for x in X_tsne]
+  y = [x[1] for x in X_tsne]
+  colors = [cl_colors[lab] for lab in labels] if (labels is not None) else 'blue'
+  plt.scatter(x, y, c=colors, alpha=0.5)
+  name = name if name else ""
+  plt.savefig('tsne_'+name+'.png')
+  plt.clf()
+  return X_tsne
+
+# greedily hash the feature space and try to get "distinct" elements
+def sub_select_hash_feature(X_tr, Y_tr, n_sub):
+  seen = set()
+  X_sub, Y_sub = [], []
+  for i in range(len(X_tr)):
+    x,y = X_tr[i], Y_tr[i]
+    feat = str(feature(x))
+    if feat not in seen:
+      X_sub.append(x)
+      Y_sub.append(y)
+      seen.add(feat)
+  return X_sub[:n_sub], Y_sub[:n_sub]
+
+def sub_select_cluster_feature(X, Y, n_samples):
+  
+  X_emb = [feature(x) for x in X]
+  from sklearn.cluster import KMeans
+  kmeans = KMeans(n_clusters=n_samples)
+  kmeans = kmeans.fit(X_emb)
+  
+  cluster_labels = list(kmeans.predict(X_emb))
+  # print (cluster_labels[:100])
+  from sklearn.metrics import pairwise_distances_argmin_min
+  closest, _ = pairwise_distances_argmin_min(kmeans.cluster_centers_, X_emb)
+  counts = [cluster_labels.count(i) for i in range(n_samples)]
+  
+  # return [ (X[closest[i]], counts[i]) for i in range(n_samples) ], kmeans.score(X_emb)
+  X_sub = []
+  Y_sub = []
+  for i in range(n_samples):
+    X_sub.append(X[closest[i]])
+    Y_sub.append(Y[closest[i]])
+  return np.array(X_sub), np.array(Y_sub)
+
+  def make_knn(self, X, Y, embed=True):
+    X_emb = self.embed(X) if embed else X
+    from sklearn import neighbors
+    # I HRD SOMEWHERE THAT USING k = LOG(len(DATA)) is good
+    clf = neighbors.KNeighborsClassifier(1+int(np.log(len(Y))))
+    # clf = neighbors.KNeighborsClassifier(1+int(np.log(len(Y))), weights='distance')
+    knn_cl = clf.fit(X_emb, Y)
+    def classify(X_new):
+      X_new_emb = self.embed(X_new) if embed else X_new
+      return knn_cl.predict(X_new_emb)
+    return classify
+
+def sub_select_knn_feature_sample(X, Y, n_samples):
+  X_emb = [feature(x) for x in X]
+
+
+# ========================= LOGISTIC REGRESSION CLASSIFIER ========================
+class LRegr:
+
+  def __init__(self):
+    self.name = "LRegr"
+
+  def learn(self, train_corpus):
+    train_data, train_label = train_corpus
+    logisticRegr = LogisticRegression(solver='sag')
+    logisticRegr.fit(train_data, train_label)
+    self.logisticRegr = logisticRegr
+
+  def evaluate(self, test_corpus):
+    test_data, test_label = test_corpus
+    label_pred = self.logisticRegr.predict(test_data)
+    return np.sum(label_pred == test_label) / len(test_label)
+
+# ======================== RUNNING THE EXPERIMENT ===========================
+def run_subset_size(X_tr, Y_tr, X_t, Y_t, n_sub):
+
+  print ("===================== running for ", n_sub, "==========================")
+  lregr = LRegr()
+  lregr.learn((X_tr, Y_tr))
+  print ( "gold_standard result : ", lregr.evaluate((X_t,Y_t)) )
+
+  X_rand, Y_rand = X_tr[:n_sub], Y_tr[:n_sub]
+  lregr = LRegr()
+  lregr.learn((X_rand, Y_rand))
+  print ( "rand_subset result : ", lregr.evaluate((X_t,Y_t)) )
+
+  X_sub, Y_sub = sub_select_hash_feature(X_tr, Y_tr, n_sub)
+  lregr = LRegr()
+  lregr.learn((X_sub, Y_sub))
+  print ( "hash_subset result : ", lregr.evaluate((X_t,Y_t)) )
+
+  X_sub, Y_sub = sub_select_cluster_feature(X_tr, Y_tr, n_sub)
+  lregr = LRegr()
+  lregr.learn((X_sub, Y_sub))
+  print ( "cluster_subset result : ", lregr.evaluate((X_t,Y_t)) )
+
+if __name__ == "__main__":
+  n = 2000
+  n_tr = n // 2
+  n_sub = 20
+
+  X, Y = make_dataset(n)
+  X_tr, Y_tr = X[:n_tr], Y[:n_tr]
+  X_t, Y_t = X[n_tr:], Y[n_tr:]
+
+  # some TSNE print for  all features and only class relevant features
+  # tsne(X_tr, Y_tr, "all_feature")
+  # X_emb = [feature(x) for x in X]
+  # tsne(X_emb, Y, "klas_feature")
+  
+  sub_sizes = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+  for sub_size in sub_sizes:
+    run_subset_size(X_tr, Y_tr, X_t, Y_t, sub_size)
+
+
+
+
