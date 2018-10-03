f416e3aa0323ee1fe0d1ceab21b70815000d569e
evanthebouncy <evanthebouncy@gmail.com>
2018-10-03 16:13:19 -0400

    added dueling knn

--- ironbox/experiments/mnist_classify.py
+++ ironbox/experiments/mnist_classify.py
@@ -13,34 +13,51 @@ def run_subset_size(clf, X_tr, Y_tr, X_t, Y_t, n_sub, ae):
     ae : the auto-encoder
   '''
   ret_dict = {
-        'name' : 'mnist_classify',
-        'n_sub' : n_sub,
-        'evaluation_model' : clf().name,
+        'meta' : {
+          'name' : 'mnist_classify',
+          'n_sub' : n_sub,
+          'evaluation_model' : clf().name,
+        },
+        'results' : {},
       }
 
+  result_dict = ret_dict['results']
+
   print ("================ running for ", n_sub, clf().name, "====================")
-  data_holder = DataHolder(X_tr, Y_tr, [1.0] * len(X_tr))
-  all_error = get_acc(clf(), data_holder, X_t, Y_t)
-  print ( "all error : ", all_error)
-  ret_dict['all'] = all_error
+  # data_holder = DataHolder(X_tr, Y_tr, [1.0] * len(X_tr))
+  # all_error = get_acc(clf(), data_holder, X_t, Y_t)
+  # print ( "all error : ", all_error)
+  # result_dict['all'] = all_error
 
   r_idx = sub_select_random(X_tr, Y_tr, n_sub)
   data_holder = DataHolder(X_tr[r_idx], Y_tr[r_idx], [1.0] * n_sub)
   rand_error = get_acc(clf(), data_holder, X_t, Y_t)
   print ( "rand error : ", rand_error)
-  ret_dict['rand'] = rand_error
+  result_dict['rand'] = rand_error
 
-  sub_idxs = sub_select_cluster(X_tr, Y_tr, n_sub, ae)
-  data_holder = DataHolder(X_tr[sub_idxs], Y_tr[sub_idxs], [1.0] * n_sub)
-  clus = get_acc(clf(), data_holder, X_t,Y_t)
-  print ( "cluster error : ", clus )
-  ret_dict['cluster'] = clus
+  # sub_idxs = sub_select_cluster(X_tr, n_sub, ae) # this cluster does not depend on Y
+  # data_holder = DataHolder(X_tr[sub_idxs], Y_tr[sub_idxs], [1.0] * n_sub)
+  # clus = get_acc(clf(), data_holder, X_t,Y_t)
+  # print ( "cluster error : ", clus )
+  # result_dict['cluster'] = clus
+
+  # sub_idxs = sub_select_label_cluster(X_tr, Y_tr, n_sub, ae) # this cluster does depend on Y
+  # data_holder = DataHolder(X_tr[sub_idxs], Y_tr[sub_idxs], [1.0] * n_sub)
+  # clus = get_acc(clf(), data_holder, X_t,Y_t)
+  # print ( "label cluster error : ", clus )
+  # result_dict['label cluster'] = clus
 
   sub_idxs = sub_select_knn(X_tr, Y_tr, n_sub, ae)
   data_holder = DataHolder(X_tr[sub_idxs], Y_tr[sub_idxs], [1.0] * n_sub)
   knnn = get_acc(clf(), data_holder, X_t,Y_t)
   print ( "knn error : ", knnn )
-  ret_dict['knn'] = knnn
+  result_dict['knn'] = knnn
+
+  sub_idxs = sub_select_knn_dueling(X_tr, Y_tr, n_sub, ae)
+  data_holder = DataHolder(X_tr[sub_idxs], Y_tr[sub_idxs], [1.0] * n_sub)
+  knnn_dueling = get_acc(clf(), data_holder, X_t,Y_t)
+  print ( "knn dueling error : ", knnn_dueling )
+  result_dict['knn_dueling'] = knnn_dueling
 
 
   return ret_dict
@@ -66,7 +83,9 @@ if __name__ == "__main__":
 
   from subset_selection.subset_selection import sub_select_cluster,\
                                                 sub_select_knn,\
-                                                sub_select_random
+                                                sub_select_label_cluster,\
+                                                sub_select_random,\
+                                                sub_select_knn_dueling
   from evaluation_models.classify_fcnet import FCNet
 
   CLFS = {
@@ -87,9 +106,9 @@ if __name__ == "__main__":
   subset_size = args.subset_size
 
 
-  result_dict = run_subset_size(CLFS[eval_model_name], 
+  experiment_dict = run_subset_size(CLFS[eval_model_name], 
                                 X_tr, Y_tr, X_t, Y_t, subset_size, ae)
-  print (result_dict)
+  print (experiment_dict)
 
 
 
