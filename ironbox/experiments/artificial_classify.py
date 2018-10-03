9654faa17edc83d6fa07ba7d868f5585c1d7d93d
evanthebouncy <evanthebouncy@gmail.com>
2018-10-03 16:25:00 -0400

    better dictionary for artificial classifications

--- ironbox/experiments/artificial_classify.py
+++ ironbox/experiments/artificial_classify.py
@@ -12,32 +12,46 @@ def run_subset_size(clf, X_tr, Y_tr, X_t, Y_t, n_sub, ae):
     n_sub : number of subset
     ae : the auto-encoder
   '''
-  ret_dict = dict()
+  ret_dict = {
+        'meta' : {
+          'name' : 'artificial_classify',
+          'n_sub' : n_sub,
+          'evaluation_model' : clf().name,
+        },
+        'results' : {},
+      }
+
+  result_dict = ret_dict['results']
 
   print ("================ running for ", n_sub, clf().name, "====================")
   data_holder = DataHolder(X_tr, Y_tr, [1.0] * len(X_tr))
   all_error = get_acc(clf(), data_holder, X_t, Y_t)
   print ( "all error : ", all_error)
-  ret_dict['all'] = all_error
+  result_dict['all'] = all_error
 
   r_idx = sub_select_random(X_tr, Y_tr, n_sub)
   data_holder = DataHolder(X_tr[r_idx], Y_tr[r_idx], [1.0] * n_sub)
   rand_error = get_acc(clf(), data_holder, X_t, Y_t)
   print ( "rand error : ", rand_error)
-  ret_dict['rand'] = rand_error
+  result_dict['rand'] = rand_error
 
-  sub_idxs = sub_select_cluster(X_tr, Y_tr, n_sub, ae)
+  sub_idxs = sub_select_cluster(X_tr, n_sub, ae) # this cluster does not depend on Y
   data_holder = DataHolder(X_tr[sub_idxs], Y_tr[sub_idxs], [1.0] * n_sub)
   clus = get_acc(clf(), data_holder, X_t,Y_t)
   print ( "cluster error : ", clus )
-  ret_dict['cluster'] = clus
+  result_dict['cluster'] = clus
+
+  sub_idxs = sub_select_label_cluster(X_tr, Y_tr, n_sub, ae) # this cluster does depend on Y
+  data_holder = DataHolder(X_tr[sub_idxs], Y_tr[sub_idxs], [1.0] * n_sub)
+  clus = get_acc(clf(), data_holder, X_t,Y_t)
+  print ( "label cluster error : ", clus )
+  result_dict['label cluster'] = clus
 
   sub_idxs = sub_select_knn(X_tr, Y_tr, n_sub, ae)
   data_holder = DataHolder(X_tr[sub_idxs], Y_tr[sub_idxs], [1.0] * n_sub)
   knnn = get_acc(clf(), data_holder, X_t,Y_t)
   print ( "knn error : ", knnn )
-  ret_dict['knn'] = knnn
-
+  result_dict['knn'] = knnn
 
   return ret_dict
 
@@ -52,7 +66,10 @@ if __name__ == "__main__":
 
   from subset_selection.subset_selection import sub_select_cluster,\
                                                 sub_select_knn,\
-                                                sub_select_random
+                                                sub_select_label_cluster,\
+                                                sub_select_random,\
+                                                sub_select_knn_dueling
+
   from evaluation_models.classify_fcnet import FCNet
 
   CLFS = {
