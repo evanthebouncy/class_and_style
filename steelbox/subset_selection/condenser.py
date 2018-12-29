4164a51d8c5f0561d51c8494b7e33f856e495d6a
evanthebouncy <evanthebouncy@gmail.com>
2018-12-28 22:14:57 -0500

    good condense algorithm

--- steelbox/subset_selection/condenser.py
+++ steelbox/subset_selection/condenser.py
@@ -101,10 +101,53 @@ def condense_once_3(X, Y, X_orig, Y_orig):
     print (" score ", score_after, " size ", len(Y_sub))
     return X_sub, Y_sub
 
+def condense_once_4(X, Y, X_orig, Y_orig, throw_frac=0.1):
+    tree = cKDTree(X)
+    _, top2_idx = tree.query(X_orig, 2)
 
+    rep_buddy = {}
 
+    def get_remove_cost(idx_rep):
+        nearests = np.where(top2_idx[:,0] == idx_rep)
+        nearests = nearests[0]
+        
+        rep_buddy[idx_rep] = set()
+
+        existing_acc = 0
+        removed_acc = 0
+        for idx_villager in nearests:
+            if Y_orig[idx_villager] == Y[idx_rep]:
+                existing_acc += 1
+
+            other_rep = top2_idx[idx_villager][1]
+            if Y_orig[idx_villager] == Y[other_rep]:
+                removed_acc += 1
+
+            # the safe removal of idx_rep is now dependent on other_rep as a buddy
+            if Y[idx_rep] == Y_orig[idx_villager] == Y[other_rep]:
+                rep_buddy[idx_rep].add(other_rep)
+
+        return existing_acc - removed_acc
+
+    remove_costs = sorted([(get_remove_cost(idx),idx) for idx in range(X.shape[0])])
+
+    throw_amt = int(max(1, throw_frac * len(Y)))
+    ban_list = set()
+    to_remove = []
+    for i in range(throw_amt):
+        throw_idx = remove_costs[i][1]
+        if throw_idx in ban_list:
+            continue
+        else:
+            to_remove.append(throw_idx)
+            ban_list.update(rep_buddy[throw_idx])
+
+    X_sub, Y_sub = np.delete(X, to_remove, 0), np.delete(Y, to_remove) 
     
-
+    W_one = np.ones(len(Y_orig))
+    score_after = score_subset(X_sub, Y_sub, X_orig, Y_orig, W_one, one_near=True)
+    print (" score ", score_after, " size ", len(Y_sub))
+    return X_sub, Y_sub
 
 
 if __name__ == '__main__':
@@ -170,9 +213,9 @@ if __name__ == '__main__':
         N = X_tr_emb.shape[0]
 
         X, Y = X_tr_emb, Y_tr
-        for i in range(70):
+        for i in range(700):
             print ("iteration ", i)
-            X, Y = condense_once_3(X, Y, X_tr_emb, Y_tr)
+            X, Y = condense_once_4(X, Y, X_tr_emb, Y_tr)
 
     test5()
 
