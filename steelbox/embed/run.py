8b0d025fc21f27ecd25f5ebe7c110bc7ce0f1996
admin <haokuan@mit.edu>
2019-01-21 19:13:09 -0500

    run.py

--- steelbox/embed/run.py
+++ steelbox/embed/run.py
@@ -173,11 +173,41 @@ def embed_mdsd_dvd():
     data_embed_path = 'data_embed/mdsd_dvd_dim{}.p'.format(emb_dim)
     pickle.dump((X_tr_emb,Y_tr), open( data_embed_path, "wb" ) )
 
+def embed_sentiment():
+    emb_dim = 32
+    from data_raw.sentiment_ import gen_data
+    X_tr, Y_tr, X_t, Y_t = gen_data("./data_raw")
+
+    import pickle
+
+    X_size = X_tr.shape[1]
+    from embed.fc_vae import FcVAE
+    vae = FcVAE(X_size, emb_dim, 'L2')
+
+    saved_model_path = 'embed/saved_models/sentiment_vae_{}.mdl'.format(emb_dim)
+    import os.path
+    if os.path.isfile(saved_model_path):
+        print ("loaded saved model at ", saved_model_path)
+        vae.load(saved_model_path)
+    else:
+        print ("no saved model found, training auto-encoder ")
+        vae.learn(X_tr, learn_iter = 100)
+        vae.save(saved_model_path)
+        print ("saved model at ", saved_model_path)
+
+    # compute the embedded features
+    X_tr_emb = vae.embed(X_tr)
+
+    data_embed_path = 'data_embed/sentiment_dim{}.p'.format(emb_dim)
+    pickle.dump((X_tr_emb,Y_tr), open( data_embed_path, "wb" ) )
+
+
 if __name__ == "__main__":
     # embed_artificial()
     # embed_mnist()
     # embed_fashion()
     # embed_mnist_usps()
     # embed_mnist_skew()
-    embed_mdsd_dvd()
+    #embed_mdsd_dvd()
+    embed_sentiment()
 
